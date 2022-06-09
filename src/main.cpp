d790d34eab28036aba5c0fd381cd59ac6ee00410
jgoed <101433036+jgoed@users.noreply.github.com>
2022-06-09 15:25:13 +0200

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,11 @@
 
 using namespace std;
 
+fstream dataset;
+
 uint32_t num_points;
 uint32_t num_dimensions;
 uint32_t num_leaders;
-uint32_t test_count = 0;
 
 struct Point
 {
@@ -26,7 +27,7 @@ struct Node
     vector<Point> points;
 };
 
-vector<uint32_t> get_random_unique_indexes(int amount, uint32_t container_size)
+vector<uint32_t> get_random_unique_indexes(uint32_t amount, uint32_t max_index)
 {
     vector<uint32_t> collected_samples;
     collected_samples.reserve(amount);
@@ -34,11 +35,11 @@ vector<uint32_t> get_random_unique_indexes(int amount, uint32_t container_size)
     unordered_set<uint32_t> visited_samples;
     random_device random_seed;        // Will be used to obtain a seed for the random number engine.
     mt19937 generator(random_seed()); // Standard mersenne_twister_engine seeded with rd().
-    int start = container_size - amount;
+    // int start = container_size - amount;
 
-    for (int j = start; j < container_size; ++j)
+    for (int i = 0; i < amount; i++)
     {
-        uniform_int_distribution<uint32_t> distribution(0, j); // To-from inclusive.
+        uniform_int_distribution<uint32_t> distribution(0, max_index); // To-from inclusive.
         uint32_t t = distribution(generator);
 
         unordered_set<uint32_t>::const_iterator iter = visited_samples.find(t);
@@ -49,11 +50,30 @@ vector<uint32_t> get_random_unique_indexes(int amount, uint32_t container_size)
         }
         else // Found
         {
-            visited_samples.insert(j);
-            collected_samples.emplace_back(j);
+            visited_samples.insert(i);
+            collected_samples.emplace_back(i);
         }
     }
     return collected_samples;
+}
+
+Point get_point_from_binary(uint32_t index)
+{
+    vector<int8_t> descriptors(num_dimensions);
+    dataset.seekg((sizeof(num_points) + sizeof(num_dimensions) + index * num_dimensions * sizeof(int8_t)), dataset.beg);
+    dataset.read(reinterpret_cast<char *>(descriptors.data()), sizeof(int8_t) * num_dimensions);
+
+    Point point;
+    point.id = index;
+    point.descriptors = descriptors;
+    return point;
+}
+
+Node get_node_from_binary(uint32_t index)
+{
+    Node node;
+    node.points.push_back(get_point_from_binary(index));
+    return node;
 }
 
 float euclidean_distance(const int8_t *a, const int8_t *b)
@@ -64,7 +84,6 @@ float euclidean_distance(const int8_t *a, const int8_t *b)
         float delta = a[i] - b[i];
         sums[i % 4] += delta * delta;
     }
-
     return sums[0] + sums[1] + sums[2] + sums[3];
 }
 
@@ -72,7 +91,6 @@ Node *get_closest_node_tree(vector<Node> &nodes, int8_t *query, int deepth)
 {
     float max = numeric_limits<float>::max();
     Node *closest = nullptr;
-
     for (Node &node : nodes)
     {
         const float distance = euclidean_distance(query, &node.points[0].descriptors[0]);
@@ -83,12 +101,10 @@ Node *get_closest_node_tree(vector<Node> &nodes, int8_t *query, int deepth)
             closest = &node;
         }
     }
-
     if (deepth > 1)
     {
         closest = get_closest_node_tree(closest->children, query, deepth - 1);
     }
-
     return closest;
 }
 
@@ -96,7 +112,6 @@ Node *get_closest_node(std::vector<Node> &nodes, int8_t *query)
 {
     float max = numeric_limits<float>::max();
     Node *closest = nullptr;
-
     for (Node &node : nodes)
     {
         const float distance = euclidean_distance(query, &node.points[0].descriptors[0]);
@@ -112,17 +127,16 @@ Node *get_closest_node(std::vector<Node> &nodes, int8_t *query)
 
 bool is_leaf(Node &node) { return node.children.empty(); }
 
-void print_index_levels(std::vector<Node> &root)
+void print_index_levels(vector<Node> &root)
 {
     // Standard level order traversal code
     // using queue
-    std::queue<Node> q; // Create a queue
-                        // Enqueue top_level
+    queue<Node> q; // Create a queue
+                   // Enqueue top_level
     for (auto &cluster : root)
     {
         q.push(cluster);
     }
-
     while (!q.empty())
     {
         int n = q.size();
@@ -136,45 +150,36 @@ void print_index_levels(std::vector<Node> &root)
 
             if (is_leaf(node))
             {
-                std::cout << " [L: " << node.points.size() << " ]";
+                cout << " [L: " << node.points.size() << " ]";
             }
             else
-                std::cout << " [N: " << node.children.size() << "] ";
+                cout << " [N: " << node.children.size() << "] ";
 
             // Enqueue all children of the dequeued item
             for (unsigned int i = 0; i < node.children.size(); i++)
                 q.push(node.children[i]);
             n--;
         }
-
-        std::cout << "\n"; // Print new line between two levels
+        cout << "\n--------------\n"; // Print new line between two levels
     }
-    std::cout << "--------------\n";
 }
 
 Node *find_nearest_leaf(int8_t *query, std::vector<Node> &nodes)
 {
     Node *closest_cluster = get_closest_node(nodes, query);
-
     if (!closest_cluster->children.empty())
     {
         return find_nearest_leaf(query, closest_cluster->children);
     }
-
     return closest_cluster;
 }
 
 int main()
 {
-    int L = 3;
+    int L = 6;
 
-    // Open dataset
-    fstream dataset("spacev1b_base.i8bin", ios::in | ios::binary);
-    if (!dataset)
-    {
-        cout << "Cannot open file!" << endl;
-        return 1;
-    }
+    // Open binary file
+    dataset.open("spacev1b_base.i8bin", ios::in | ios::binary);
 
     // Read metadata from binary file
     dataset.read((char *)&num_points, sizeof(uint32_t));
@@ -184,56 +189,32 @@ int main()
     int desired_cluster_size = 512000;                                                                              // 512000 byte is default block size for SSDs
     num_leaders = ceil(num_points / (desired_cluster_size / (sizeof(int8_t) * num_dimensions + sizeof(uint32_t)))); // N/ts
 
+    // FIXME Sometimes segmentation vault
     // Generate random leaders
-    // vector<uint32_t> random_leader_indexes = get_random_unique_indexes(num_leaders, num_points);
+    // vector<uint32_t> random_leader_indexes = get_random_unique_indexes(num_leaders, num_points-1);
 
+    // Tree index structure
     vector<Node> tree;
 
     for (int cur_lvl = 1; cur_lvl <= L; cur_lvl++)
     {
-        int leaders_per_lvl = ceil(pow(num_leaders, ((1.0 / L)) * (cur_lvl)));
+        // Calculate number of leaders for current level
+        int cur_lvl_leaders = ceil(pow(num_leaders, ((1.0 / L)) * (cur_lvl)));
 
-        if (cur_lvl == 1)
+        if (cur_lvl == 1) // If current level is top level
         {
             vector<Node> current_level;
-
-            for (int i = 0; i < leaders_per_lvl; i++)
+            for (int i = 0; i < cur_lvl_leaders; i++)
             {
-                // Read descriptors at index
-                vector<int8_t> current_descriptors(num_dimensions);
-                dataset.seekg((sizeof(num_points) + sizeof(num_dimensions) + i * num_dimensions * sizeof(int8_t)), dataset.beg);
-                dataset.read(reinterpret_cast<char *>(current_descriptors.data()), sizeof(int8_t) * num_dimensions);
-
-                // Create point with descriptors
-                Point current_point;
-                current_point.id = i;
-                current_point.descriptors = current_descriptors;
-
-                // Create node with point
-                Node current_node;
-                current_node.points.push_back(current_point);
-                current_level.push_back(current_node);
+                current_level.push_back(get_node_from_binary(i));
             }
             tree.swap(current_level);
         }
-        else
+        else // Every other level below top level
         {
-            for (int i = 0; i < leaders_per_lvl; i++)
+            for (int i = 0; i < cur_lvl_leaders; i++)
             {
-                // Read descriptors at index
-                vector<int8_t> current_descriptors(num_dimensions);
-                dataset.seekg((sizeof(num_points) + sizeof(num_dimensions) + i * num_dimensions * sizeof(int8_t)), dataset.beg);
-                dataset.read(reinterpret_cast<char *>(current_descriptors.data()), sizeof(int8_t) * num_dimensions);
-
-                // Create point with descriptors
-                Point current_point;
-                current_point.id = i;
-                current_point.descriptors = current_descriptors;
-
-                // Create node with point
-                Node current_node;
-                current_node.points.push_back(current_point);
-
+                Node current_node = get_node_from_binary(i);
                 Node *clostest_node = get_closest_node_tree(tree, &current_node.points[0].descriptors[0], cur_lvl - 1);
                 clostest_node->children.push_back(current_node);
             }
@@ -241,33 +222,17 @@ int main()
     }
 
     // Add all points from input dataset to the index incl those duplicated in the index construction.
-    
-    for (uint32_t id = 0; id < num_points; id++)
+    for (uint32_t cur_index = 0; cur_index < num_points; cur_index++)
     {
-        // Read descriptors at index
-        vector<int8_t> current_descriptors(num_dimensions);
-        dataset.seekg((sizeof(num_points) + sizeof(num_dimensions) + id * num_dimensions * sizeof(int8_t)), dataset.beg);
-        dataset.read(reinterpret_cast<char *>(current_descriptors.data()), sizeof(int8_t) * num_dimensions);
-
-        // Create point with descriptors
-        Point current_point;
-        current_point.id = id;
-        current_point.descriptors = current_descriptors;
-
-        auto *leaf = find_nearest_leaf(current_point.descriptors.data(), tree);
-        // Because the leader was added to the cluster when the index was built
-        if (id != leaf->points[0].id)
+        Point cur_point = get_point_from_binary(cur_index);
+        auto *leaf = find_nearest_leaf(cur_point.descriptors.data(), tree);
+        if (cur_index != leaf->points[0].id) // Prevent adding leader twice
         {
-            leaf->points.emplace_back(current_point);
+            leaf->points.emplace_back(cur_point);
         }
-        test_count++;
     }
 
-    cout << test_count << endl;
-
     print_index_levels(tree);
-
-    cout << sizeof(tree) << endl;
 
     return 0;
 }
