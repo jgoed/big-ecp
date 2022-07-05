4d0dc80da63e80eaeab202fba009f4b0ed7a2936
jgoed <101433036+jgoed@users.noreply.github.com>
2022-07-05 11:11:52 +0200

    Changed Dockerfile.ecp

---
+++ scripts/copy_files.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+#Copies necessary files into big-ann-benchmarks repository
+cp ../benchmark/ecp-t2.py ../../big-ann-benchmarks/benchmark/algorithms/ecp-t2.py
+cp ../benchmark/Dockerfile.ecp ../../big-ann-benchmarks/install/Dockerfile.ecp
+cp ../benchmark/algos.yaml ../../big-ann-benchmarks/algos.yaml
