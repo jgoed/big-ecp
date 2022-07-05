bd70018557fb136dde004560d65cc5a98d3f467c
jgoed <101433036+jgoed@users.noreply.github.com>
2022-07-05 19:00:59 +0200

    Updated ecp-t2.py

--- benchmark/ecp-t2.py
+++ benchmark/ecp-t2.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
-import benchmark.algorithms.ecp_wrapper
+from operator import index
+import benchmark.algorithms.ecp_wrapper as ecp
 
 from benchmark.algorithms.base import BaseANN
 from benchmark.datasets import DATASETS
@@ -25,8 +26,9 @@ class Ecp(BaseANN):
     def fit(self, dataset):
         ds = DATASETS[dataset]()
         dataset_file_path = ds.get_dataset_fn()
-        print("FIT")
-        #ecp_wrapper.ecp_create_index()
+        self.querie_file_path = ds.qs_fn
+        self.index_file_path = ecp.ecp_create_index(dataset_file_path, self.L, self.DCS)
+        self.meta_data_file_path = ecp.ecp_assign_points_to_cluster(dataset_file_path, self.index_file_path, 5000000)
 
     def load_index(self, dataset):
         return False
@@ -35,7 +37,19 @@ class Ecp(BaseANN):
         return False
     
     def query(self, X, k):
-        return False
+        result = ecp.ecp_process_query("data/MSSPACEV1B/query.i8bin", self.index_file_path, self.meta_data_file_path, self.k, self.b, self.L)
+        self.res = result
+
+    def get_results(self):
+        """
+        Helper method to convert query results of k-NN search.
+        If there are nq queries, returns a (nq, k) array of integers
+        representing the indices of the k-NN for each query.
+        """
+        return self.res
 
     def set_query_arguments(self, query_args):
-        print("SET QUERY ARGS")
+        self._query_args = query_args
+        self.k = query_args.get("k")
+        self.b = query_args.get("b")
+        self.L = query_args.get("L")
