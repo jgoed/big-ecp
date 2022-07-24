076e86f0dd2b23b2ea63bd9d706ab6aa47d0f83e
jgoed <101433036+jgoed@users.noreply.github.com>
2022-07-24 14:48:06 +0200

    Add comments in datastructures.hpp

--- src/datastructures.cpp
+++ src/datastructures.cpp
@@ -1,7 +1,7 @@
 #include "datastructures.hpp"
 
-namespace globals
+namespace globals // Global variables
 {
-    const float FLOAT_MAX = std::numeric_limits<float>::max();
-    uint32_t DIST_CALCULATIONS;
+    const float FLOAT_MAX = std::numeric_limits<float>::max(); // Max value a float can hold, needed to initalice threshold for distance computation
+    uint32_t DIST_CALCULATIONS;                                // Counter for number of distance computation, can only be used with single core, because atomic overhead for multithreading slows everything down
 }
