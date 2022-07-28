f573728fd60f1a2b710645a06817f9a901fedc27
jgoed <101433036+jgoed@users.noreply.github.com>
2022-07-28 11:27:17 +0200

    Test

--- src/datastructures.cpp
+++ src/datastructures.cpp
@@ -3,5 +3,5 @@
 namespace globals // Global variables
 {
     const float FLOAT_MAX = std::numeric_limits<float>::max(); // Max value a float can hold, needed to initalice threshold for distance computation
-    uint32_t DIST_CALCULATIONS = 0;                            // Counter for number of distance computation, can only be used with single core, because atomic overhead for multithreading slows everything down
+    uint32_t DIST_CALCULATIONS;                            // Counter for number of distance computation, can only be used with single core, because atomic overhead for multithreading slows everything down
 }
